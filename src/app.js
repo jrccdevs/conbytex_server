d5f13952c54bc381435944a84944417cc354046f
rei <reynaldocalani80@gmail.com>
2025-12-20 14:41:36 -0400

    correccion de cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config({ path: '../.env' });
+
 
 const FRONTEND_URL = process.env.FRONTEND_URL;
 // 🔹 Middleware

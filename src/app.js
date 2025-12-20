b589e3a4396aaf55eacaf23d644923ca654b4b63
rei <reynaldocalani80@gmail.com>
2025-12-20 14:29:06 -0400

    se desplego la aplicacion en vercel

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const FRONTEND_URL = process.env.FRONTEND_URL;
 // 🔹 Middleware
 app.use(cors({
-  origin: 'http://localhost:5173', // URL de tu frontend
+  origin:[ 'http://localhost:5173', FRONTEND_URL ], // URL de tu frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../.env') });
+
 const app = require('./app');
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en http://localhost:${PORT}`);

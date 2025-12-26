66a2b4b077183c70dbc5280fd65d96cf9f04fda8
rei <reynaldocalani80@gmail.com>
2025-12-25 21:13:42 -0400

    retrocedemos un paso antrior de inventario

--- src/controllers/inventario.controller.js
+++ src/controllers/inventario.controller.js
@@ -1,21 +1,16 @@
 const Inventario = require("../models/inventario.model");
 
 const inventarioController = {
-    // Listar stock por almacén
     listarStockAlmacen: async (req, res) => {
         const { id_almacen } = req.params;
-
         try {
-            // Obtenemos el stock desde el modelo
             const stock = await Inventario.obtenerStockPorAlmacen(id_almacen);
-
-            // Devolvemos resultado
             res.status(200).json(stock);
         } catch (error) {
             console.error("Error en inventarioController:", error);
-            res.status(500).json({
+            res.status(500).json({ 
                 mensaje: "Error al obtener el stock del almacén",
-                error: error.message
+                error: error.message 
             });
         }
     }
